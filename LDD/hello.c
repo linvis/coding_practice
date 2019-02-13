6055765e2e2fe7e079851d96834280fe06b1b30e
shlinym <jarvisfironman@gmail.com>
2019-02-13 14:55:57 +0800

    hello module finished

--- LDD/hello.c
+++ LDD/hello.c
@@ -3,18 +3,17 @@
  */
 #include <linux/init.h>
 #include <linux/module.h>
-
 MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-    printk(KERN_ALERT "hello world\n");
-    return 0;
+	printk(KERN_ALERT "Hello, world\n");
+	return 0;
 }
 
 static void hello_exit(void)
 {
-    printk(KERN_ALERT "Bye-bye\n");
+	printk(KERN_EMERG "Goodbye, cruel world\n");
 }
 
 module_init(hello_init);
